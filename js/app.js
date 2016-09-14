4bb68887d1f782bd54e8444cc8ba9542185c0c84
Kathure <lizkathure@gmail.com>
2016-09-14 13:23:01 +0300

    sticky responsive navigation bar and smooth scroll

--- js/app.js
+++ js/app.js
@@ -1,14 +1,41 @@
 $(document).foundation()
+
+//smooth scroll function
+
 $(function () {
-
-    // Returns width of browser viewport
-    var browser = $(window).width();
-    // Returns width of HTML document
-    var document = $(document).width();
-
-    $('.nav .toggle-nav').click(function (e) {
-        $('.nav .nav-mobile').addClass('style-mobile').slideToggle('slow');
-        e.preventDefault();
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
     });
+});
 
+//sticky navigation bar function
+
+
+$(document).ready(function () {
+    var stickyNavTop = $('.navigation').offset().top;
+
+    var stickyNav = function () {
+        var scrollTop = $(window).scrollTop();
+
+        if (scrollTop > stickyNavTop) {
+            $('.navigation').addClass('sticky');
+        } else {
+            $('.navigation').removeClass('sticky');
+        }
+    };
+
+    stickyNav();
+
+    $(window).scroll(function () {
+        stickyNav();
+    });
 });
